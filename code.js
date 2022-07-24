5f1ec56587610021689cd5e2d1fd7abf43c6fc03
arka1002 <arka.mcu700@gmail.com>
2022-07-24 13:25:04 +0530

    Initial Commit

---
+++ code.js
@@ -0,0 +1,47 @@
+// hero text animation
+new TypeIt('#hero', {
+    speed: 50,
+    loop: true,
+    startDelay: 900,
+    lifeLike: true,
+    nextStringDelay: 1000
+})
+.type("hello welcome to", { delay: 100 })
+.move(-11, { delay: 100 })
+.type("!", { delay: 400 })
+.move(null, { to: "START", instant: true, delay: 300 })
+.move(1, { delay: 200 })
+.delete(1)
+.type("H", { delay: 225 })
+.move(7)
+.delete(1)
+.type("W", { delay: 225 })
+.move(null, { to: "END" })
+.type(" my Portfolio.", { delay: 3000 })
+.go();
+
+
+// back to top button
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
